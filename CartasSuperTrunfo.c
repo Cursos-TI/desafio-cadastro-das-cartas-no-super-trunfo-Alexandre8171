c2781242f81134251b92d5d35fa78085bb70ec17
Alexandre <absprogramador@gmail.com>
2025-07-05 17:00:45 +0000

    primeira CartasSuperTrunfo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -5,8 +5,22 @@
 // Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
 // Siga os comentários para implementar cada parte do desafio.
 //Teste larissa
+int main (){
+char letra = 'A'; // representa um estado
+    char estado [10] = "A01"; // código de cada estado
+    char cidade [20] = "Rio de Janeiro"; // nome da cidade
+    double população = 6211.42; //população
+    float área = 1255.3; //Área por KM2
+    float pib = 1.052; // Produto Interno Bruto
+    int pontos = 58; //pontos turisticos
+ 
+    printf("Carta 1:\nestado: %c\ncodigo: %s\n",letra, estado); 
+      printf("Nome da Cidade: %s\nPopulação: %.2f\nÁrea: %2d KM2\n",cidade, população, área);
+         printf("PIB: %.3f Trilhões de Reais\nNúmero de Pontos Turisticos: %d\n",pib, pontos);
+            return 0;
+}
 
-int main() {
+
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     
@@ -18,5 +32,4 @@ int main() {
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
 
-    return 0;
-}
+    
